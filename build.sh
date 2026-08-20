1238a5e2d0588b0824902a49e9fd0855e5fd9b0e
Natchlazen <gempro760@gmail.com>
2026-08-20 15:03:00 -0500

    Configurar base de datos y actualizar script de construcción

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Descarga uv e instala dependencias
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install
+make install && psql -a -d $DATABASE_URL -f database.sql
